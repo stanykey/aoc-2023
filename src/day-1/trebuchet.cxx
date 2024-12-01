28ebd1fe72715ca61f22c3f03c369b1dc84ef22f
Sergii Lovygin <lovygin.dev@gmail.com>
2024-12-01 16:13:29 +0200

    Add the description of 2nd puzzle for day 1

--- src/day-1/trebuchet.cxx
+++ src/day-1/trebuchet.cxx
@@ -1,50 +1,64 @@
 #include <algorithm>
-#include <cctype>
 #include <cstdint>
 #include <format>
 #include <fstream>
 #include <iostream>
 #include <iterator>
 #include <numeric>
+#include <optional>
+#include <regex>
 #include <string>
-#include <string_view>
+#include <tuple>
 #include <vector>
 
 
-auto load_data(const std::string& path) -> std::vector<std::string> {
-    std::ifstream file(path);
+namespace {
+    auto load_data(const std::string& path) -> std::vector<std::string> {
+        std::ifstream file{path};
 
-    std::vector<std::string> data;
-    std::copy(std::istream_iterator<std::string>(file), std::istream_iterator<std::string>(), std::back_inserter(data));
-    return data;
-}
+        std::vector<std::string> data;
+        std::copy(std::istream_iterator<std::string>(file), std::istream_iterator<std::string>(), std::back_inserter(data));
+        return data;
+    }
+
+    auto get_calibration_digits(const std::string& str) -> std::optional<std::tuple<std::int32_t, std::int32_t>> {
+        const auto pattern = std::regex{R"(\d)"};
+
+        auto matches = std::vector<std::string>{};
+        auto begin   = std::sregex_iterator{str.cbegin(), str.cend(), pattern};
+        for (const auto end = std::sregex_iterator{}; begin != end; ++begin) {
+            matches.emplace_back(begin->str());
+        }
+
+        if (matches.empty()) {
+            // No digits found
+            return std::nullopt;
+        }
+
+        const auto first_digit = matches.front()[0] - '0';
+        const auto last_digit  = matches.back()[0] - '0';
+        return std::make_tuple(first_digit, last_digit);
+    }
 
 
-constexpr auto find_first_digit(std::string_view str) -> std::int32_t {
-    const auto it = std::find_if(str.cbegin(), str.cend(), [](const char ch) { return std::isdigit(ch); });
-    return (it == str.cend()) ? 0 : *it - '0';
-}
+    auto get_calibration_value(const std::string& str) -> std::int32_t {
+        const auto digits = get_calibration_digits(str);
+        if (!digits) {
+            return 0;
+        }
+
+        const auto [first, second] = digits.value();
+        return first * 10 + second;  // NOLINT: no reasonable name for 10 `magic` number
+    }
 
 
-constexpr auto find_last_digit(std::string_view str) -> std::int32_t {
-    const auto it = std::find_if(str.crbegin(), str.crend(), [](const char ch) { return std::isdigit(ch); });
-    return (it == str.crend()) ? 0 : *it - '0';
-}
-
-
-constexpr auto get_calibration_value(std::string_view data) -> std::int32_t {
-    const auto first = find_first_digit(data);
-    const auto last  = find_last_digit(data);
-    return first * 10 + last;  // NOLINT: no reasonable name for 10 `magic` number
-}
-
-
-auto get_calibration_values(const std::vector<std::string>& data) -> std::vector<std::int32_t> {
-    std::vector<std::int32_t> calibrations;
-    calibrations.reserve(data.size());
-    std::ranges::transform(data, std::back_inserter(calibrations), get_calibration_value);
-    return calibrations;
-}
+    auto get_calibration_values(const std::vector<std::string>& data) -> std::vector<std::int32_t> {
+        std::vector<std::int32_t> calibrations;
+        calibrations.reserve(data.size());
+        std::ranges::transform(data, std::back_inserter(calibrations), get_calibration_value);
+        return calibrations;
+    }
+}  // namespace
 
 
 auto main() -> int {
