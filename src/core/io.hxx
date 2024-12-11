10fb103fb7e267845f75a1cf7cff554af0cc4ca5
Sergii Lovygin <lovygin.dev@gmail.com>
2024-12-11 12:18:15 +0200

    Add overload of the `read_file` for `std::filesystem::path`

--- src/core/io.hxx
+++ src/core/io.hxx
@@ -37,7 +37,13 @@ namespace core::io {
     }
 
     auto read_line(std::istream& stream) -> std::string;
+
     auto read_file(const std::string& path, bool as_text) -> std::string;
+
+    template<typename Path>
+    auto read_file(const Path& path, bool as_text) -> std::string {
+        return read_file(path.string(), as_text);
+    }
 }  // namespace core::io
 
 #endif  // CORE_IO_HXX
